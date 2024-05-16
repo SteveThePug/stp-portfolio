d133cf6c4d302d367a41bb94e04bf3ab9950f229
Adam French <adam.a.french@outlook.com>
2024-05-16 11:04:14 +0100

    added tailwind css

---
+++ .nuxt/app.config.mjs
@@ -0,0 +1,20 @@
+
+import { updateAppConfig } from '#app/config'
+import { defuFn } from 'defu'
+
+const inlineConfig = {
+  "nuxt": {
+    "buildId": "7e9ace58-cc53-490b-8d71-8b20c520967a"
+  }
+}
+
+// Vite - webpack is handled directly in #app/config
+if (import.meta.hot) {
+  import.meta.hot.accept((newModule) => {
+    updateAppConfig(newModule.default)
+  })
+}
+
+
+
+export default /*@__PURE__*/ defuFn(inlineConfig)
